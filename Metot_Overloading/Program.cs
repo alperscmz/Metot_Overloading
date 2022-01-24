347d24caf02349b1f6f0a5d6af2cbf9e637c81e6
alperscmz <alpersacmaozu@gmail.com>
2022-01-24 18:33:22 +0300

    Metot_Overloading
    vsc c# ile Metot Overloading

---
+++ Metot_Overloading/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Metot_Overloading
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+          //out parametreler
+
+          string sayi="777";
+          int outSayi;
+
+         bool sonuc= int.TryParse(sayi,out outSayi);
+         if(sonuc)
+         {
+             Console.WriteLine("Başarılı.");
+              Console.WriteLine(outSayi);
+         }
+         else
+         {
+             Console.WriteLine("Başarısız.");
+         }
+
+       
+        Metotlar instance=new Metotlar();
+        instance.Topla(4,5, out int toplamSonucu);
+           Console.WriteLine(toplamSonucu);
+
+           //Metot aşırı yükleme-overloading
+
+           int ifade=999;
+           instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("alper ","saçmaözü");
+    }
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int toplam)
+        {
+           toplam=a+b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
